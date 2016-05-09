a8b9ccf2ca7536aba375f431eb57ee263b2ac208
Gertjan Roke <gjroke@intothesource.com>
2016-05-09 15:05:57 +0200

    Default role option in the config file

--- src/config/intothesource.usermanager.php
+++ src/config/intothesource.usermanager.php
@@ -6,12 +6,20 @@
          * Multiple roles per user
          */
         'multiple' => false,
+
+        /**
+         * Set the default role when a user register with the entrance package
+         * @string
+         */
+        'default_role' => 'admin',
+
         /**
          * Prefix your url (The route would look something like this 'cms/users')
          *
          * @note add a '/' at the end of the prefix
          */
         'prefix' => 'cms/',
+
         /**
          * Add all the roles that have access to the users table and role table
          */
